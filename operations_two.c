efb93bcf123704891b07f52cc67365105e643c04
Muffin <amiryou120@gmail.com>
2023-06-22 06:55:40 -0700

    operations at its finest!

--- operations_two.c
+++ operations_two.c
@@ -37,3 +37,32 @@ void _mod(stack_t **head, unsigned int count)
 	*head = h->next;
 	free(h);
 }
+/**
+ * _swap - swaps
+ * @head: stack head
+ * @count: number of lines
+*/
+void _swap(stack_t **head, unsigned int count)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", count);
+		fclose(alx.file);
+		free(alx.buf);
+		_free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	aux = h->n;
+	h->n = h->next->n;
+	h->next->n = aux;
+}
